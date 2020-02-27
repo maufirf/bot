164536a408fe24109d6b9bacfd4a6340e8357721
Muhammad Aufi Rayesa Frandhana <parampaa2@gmail.com>
2020-02-27 17:48:51 +0700

    added twitter | twitter.com/really_a_bot

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import os
 import json
 import facebook
+import tweepy
 import stringer
+
+print('IMPORT FINISHED')
 
 ENV = None
 if os.path.exists('auth.json'):
@@ -11,11 +14,30 @@ if os.path.exists('auth.json'):
 else:
     ENV = {
         "FB_ACC_TOKEN_PAINTMIN":os.environ.get('FB_ACC_TOKEN_PAINTMIN'),
-        "PAGE_ID":os.environ.get('PAGE_ID')
+        "PAGE_ID":os.environ.get('PAGE_ID'),
+        "TWITTER_CONSUMER_KEY":os.environ.get('TWITTER_CONSUMER_KEY'),
+        "TWITTER_CONSUMER_SECRET":os.environ.get('TWITTER_CONSUMER_SECRET'),
+        "TWITTER_ACC_TOKEN":os.environ.get('TWITTER_ACC_TOKEN'),
+        "TWITTER_ACC_TOKEN_SECRET":os.environ.get('TWITTER_ACC_TOKEN_SECRET'),
     }
+print('TOKEN LOADED')
 
 stringer_ctrl = stringer.Stringer_Ctrl()
+print('STRINGER OBJECT INITIALIZED')
+generated = stringer_ctrl.generate()
+print(f'NEW TEXT GENERATED: {generated}')
 
 graph = facebook.GraphAPI(access_token=ENV['FB_ACC_TOKEN_PAINTMIN'])
-graph.put_object(parent_object='me', connection_name='feed',
+print('FACEBOOK GRAPH OBJECT CREATED',graph,sep='\n')
+
+auth = tweepy.OAuthHandler(ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"])
+auth.set_access_token(ENV["TWITTER_ACC_TOKEN"], ENV["TWITTER_ACC_TOKEN_SECRET"])
+api = tweepy.API(auth)
+print('TWITTER API OBJECT CREATED',api,sep='\n')
+
+fb_response = graph.put_object(parent_object='me', connection_name='feed',
                   message=stringer_ctrl.generate())
+print('FACEBOOK RESPONSE: ',fb_response,sep='\n')
+
+twitter_response = api.update_status(generated)
+print('TWITTER RESPONSE: ',twitter_response,sep='\n')
